67e65dad2ddc549f30771a20c5892777d043b800
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-05-08 20:16:28 +0300

    implements basic reading functionality by rows

--- Table.cpp
+++ Table.cpp
@@ -1,1 +1,32 @@
 #include "Table.h"
+
+static int countCharacterOccurances(std::ifstream& ifs, char ch)
+{
+	unsigned counter = 0;
+	int originalPosition = ifs.tellg();
+	ifs.seekg(0, std::ios::beg);
+	while (!ifs.eof())
+	{
+		if (ifs.get() == ch) counter++;
+	}
+
+	ifs.seekg(originalPosition);
+	return counter;
+}
+
+void Table::readFromFile(const char* fileName)
+{
+	std::ifstream ifs(fileName, std::ios::in);
+
+	if (!ifs.is_open())
+		throw std::invalid_argument("File not found!");
+
+	size_t linesCount = countCharacterOccurances(ifs, '\n');
+	_rowsCount = linesCount;
+	_rows = new Row[linesCount];
+
+	for (int i = 0; i < linesCount; i++)
+	{
+		_rows[i].readRowFromFile(ifs);
+	}
+}
