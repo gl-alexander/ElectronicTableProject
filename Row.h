e3ba6e5a71df7d848f9bd184144918a2df3ecc20
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-05-18 01:50:47 +0300

    implements vector for storing the values

--- Row.h
+++ Row.h
@@ -1,30 +1,20 @@
 #pragma once
 #include "Cell.h"
+#include "MyVector.hpp"
 #include <fstream>
-
 
 class Row
 {
-	Cell* _cells = nullptr;
+	MyVector<Cell> _cells;
 	size_t _cellCount = 0;
 
-	void copyFrom(const Row& other);
-	void free();
-	void moveFrom(Row&& other);
-
-	
 public:
-	Row() = default;
-	Row(const Row& other);
-	Row& operator=(const Row& other);
-	~Row();
-
-	Row(Row&& other);
-	Row& operator=(Row&& other);
-
 	void readRowFromFile(std::ifstream& ifs);
 
 	Cell& operator[](int ind);
 	const Cell& operator[](int ind) const;
+
+	//to be removed:
+	void printValueTypes() const;
 };
 
