40cf6fc9b84a878732d00740686d9997502ad251
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-05-18 01:53:14 +0300

    implements vector for stroing the rows

--- Table.cpp
+++ Table.cpp
@@ -10,63 +10,8 @@ static int countCharacterOccurances(std::ifstream& ifs, char ch)
 		if (ifs.get() == ch) counter++;
 	}
 	ifs.clear();
-	ifs.seekg(std::ios::beg);
+	ifs.seekg(originalPosition);
 	return counter;
-}
-
-void Table::copyFrom(const Table& other)
-{
-	_rowsCount = other._rowsCount;
-	_rows = new Row[other._rowsCount];
-	for (int i = 0; i < other._rowsCount; i++)
-	{
-		_rows[i] = other._rows[i];
-	}
-}
-void Table::free()
-{
-	delete[] _rows;
-	_rows = nullptr;
-	_rowsCount = 0;
-}
-void Table::moveFrom(Table&& other)
-{
-	_rows = other._rows;
-	_rowsCount = other._rowsCount;
-	delete[] other._rows;
-}
-
-
-Table::Table(const Table& other)
-{
-	copyFrom(other);
-}
-Table& Table::operator=(const Table& other)
-{
-	if (this != &other)
-	{
-		free();
-		copyFrom(other);
-	}
-	return *this;
-}
-Table::~Table() noexcept
-{
-	free();
-}
-
-Table::Table(Table&& other) noexcept
-{
-	moveFrom(std::move(other));
-}
-Table& Table::operator=(Table&& other) noexcept
-{
-	if (this != &other)
-	{
-		free();
-		moveFrom(std::move(other));
-	}
-	return *this;
 }
 
 void Table::readFromFile(const char* fileName)
@@ -77,9 +22,9 @@ void Table::readFromFile(const char* fileName)
 		throw std::runtime_error("File not found!");
 
 	size_t linesCount = countCharacterOccurances(ifs, '\n');
-	std::cout << linesCount << std::endl;
+	
 	_rowsCount = linesCount + 1;
-	_rows = new Row[_rowsCount];
+	_rows = MyVector<Row>(_rowsCount);
 
 	for (int i = 0; i < _rowsCount; i++)
 	{
@@ -89,7 +34,7 @@ void Table::readFromFile(const char* fileName)
 		}
 		catch (std::invalid_argument& ex)
 		{
-			std::cout << "Error: row " << i << ", " << ex.what() << " Unknown data type";
+			std::cout << "Error: row " << i << ", " << ex.what() << " is unknown data type\n";
 		}
 	}
 }
