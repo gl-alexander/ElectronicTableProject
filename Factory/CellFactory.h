f16f42a6bb371db3cad792a9259988cb7b9960a8
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-07 01:10:52 +0300

    Update CellFactory.h

--- Factory/CellFactory.h
+++ Factory/CellFactory.h
@@ -1,8 +1,19 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include "../Cells/IncludeCells.h"
+
+const char COMMA_SYMBOL = '.';
 
 class CellFactory
 {
+	static const CellType& getType(const char* str);
 
+	static Cell* createIntegerCell(std::stringstream& ss);
+	static Cell* createStringCell(std::stringstream& ss);
+	static Cell* createFractionCell(std::stringstream& ss);
+	static Cell* createFormulaCell(std::stringstream& ss);
+
+public:
+	static Cell* createCell(std::stringstream& ss);
 };
