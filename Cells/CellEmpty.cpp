a50ef869f5141f5fde7d88091668657eff0e2a6a
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-12 02:24:19 +0300

    adds clone()

--- Cells/CellEmpty.cpp
+++ Cells/CellEmpty.cpp
@@ -5,3 +5,8 @@ void CellEmpty::printCell(size_t len, std::ostream& os) const
 	PrintHelper::printWhitespaces(len, os);
 	os << PRINT_SEPARATOR;
 }
+
+Cell* CellEmpty::clone() const
+{
+	return new CellEmpty(*this);
+}
