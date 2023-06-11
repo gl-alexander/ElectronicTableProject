df9bf9aeff758a67bcffe06d3b11b49adaea6892
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-12 02:15:42 +0300

    adds printCell

--- Cells/CellInteger.cpp
+++ Cells/CellInteger.cpp
@@ -1,1 +1,11 @@
 #include "CellInteger.h"
+
+CellInteger::CellInteger(int value) : _value(value) {}
+
+void CellInteger::printCell(size_t len, std::ostream& os) const
+{
+	size_t valueLen = PrintHelper::intLen(_value);
+	os << _value;
+	PrintHelper::printWhitespaces(len - valueLen, os);
+	os << PRINT_SEPARATOR;
+}
