c9993077eafdfd55a8b7c475d70f13a746782ecc
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-12 02:16:04 +0300

    adds printCell

--- Cells/CellString.cpp
+++ Cells/CellString.cpp
@@ -1,1 +1,10 @@
 #include "CellString.h"
+
+CellString::CellString(const MyString& value) : _value(value) {}
+
+void CellString::printCell(size_t len, std::ostream& os) const
+{
+	os << _value;
+	PrintHelper::printWhitespaces(len - _value.length(), os);
+	os << PRINT_SEPARATOR;
+}
