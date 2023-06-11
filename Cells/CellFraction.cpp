cedf6895ff5bcdb3d2763b496f87f3d6b7e8f022
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-12 02:15:19 +0300

    adds printCell

--- Cells/CellFraction.cpp
+++ Cells/CellFraction.cpp
@@ -1,1 +1,12 @@
 #include "CellFraction.h"
+
+CellFraction::CellFraction(double value) : _value(value) {}
+
+void CellFraction::printCell(size_t len, std::ostream& os) const
+{
+	os.precision(PrintHelper::DECIMAL_PLACES_TO_PRINT);
+	size_t valueLen = PrintHelper::doubleLen(_value, PrintHelper::DECIMAL_PLACES_TO_PRINT);
+	os << _value;
+	PrintHelper::printWhitespaces(len - valueLen, os);
+	os << PRINT_SEPARATOR;
+}
