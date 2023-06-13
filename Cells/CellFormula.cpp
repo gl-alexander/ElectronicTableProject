d904c241949c7a3e60084a63e631f3a0c401cfa1
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-13 14:01:11 +0300

    adds constructor for storing the private member with the expression

--- Cells/CellFormula.cpp
+++ Cells/CellFormula.cpp
@@ -1,1 +1,14 @@
 #include "CellFormula.h"
+
+CellFormula::CellFormula(const MyString& expression) : _expression(expression) {}
+
+void CellFormula::parseInput(std::stringstream& ss)
+{
+
+}
+
+double CellFormula::evaluate() const
+{
+	return 0;
+	// go through stack and evaluate everything and add operators
+}
