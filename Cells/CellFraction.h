c032dc89cc20d177cdfc1ce2a7ba517cbb900e28
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-13 14:01:54 +0300

    adds evaluate()

--- Cells/CellFraction.h
+++ Cells/CellFraction.h
@@ -6,14 +6,18 @@ class CellFraction : public Cell
 {
 	double _value;
 public:
-	CellFraction();
+	CellFraction() = default;
 
-	void printCell(size_t len) const override;
+	CellFraction(double value);
+
+	void printCell(size_t len, std::ostream& os) const override;
 
 	Cell* clone() const override;
 
 	void setValue(double value);
 
 	double getValue() const;
+
+	double evaluate() const override;
 };
 
