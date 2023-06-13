70a2f1c49677640e4806b93daee549c264984d76
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-13 14:00:42 +0300

    adds evaluate and private member for storing the formula expression

--- Cells/CellFormula.h
+++ Cells/CellFormula.h
@@ -1,25 +1,33 @@
 #pragma once
 #include <iostream>
 #include "Cell.h"
-#include "../MyVector.hpp"
+#include "../Utilities/MyVector.hpp"
 
 class CellFormula : public Cell
 {
 	//stack of cells
 	//stack of operators
+	MyString _expression;
+
 	MyVector<Cell> _cells;
 	MyVector<char> _operators;
 
 	Cell* _left;
 	Cell* _right;
 public:
-	CellFormula();
+	CellFormula() = default;
 
-	void printCell(size_t len) const override;
+	CellFormula(const MyString& expression);
+
+	void printCell(size_t len, std::ostream& os) const override;
 
 	Cell* clone() const override;
 
 	void setValue(const MyString& value);
+
+	void parseInput(std::stringstream& ss);
+
+	double evaluate() const override;
 
 	double getValue() const;
 };
