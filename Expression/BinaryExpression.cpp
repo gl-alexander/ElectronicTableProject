0cf8c808be46dddabb756f880179840f265e9cb6
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-14 22:58:28 +0300

    adds exception throwing for division

--- Expression/BinaryExpression.cpp
+++ Expression/BinaryExpression.cpp
@@ -1,21 +1,40 @@
 #include "BinaryExpression.h"
 
-BinaryExpression::BinaryExpression(Expression* left, Expression* right, char operation) : left(left), right(right), operation(operation)
+BinaryExpression::BinaryExpression(char operation, Expression* left, Expression* right) : left(left), right(right), operation(operation)
 {}
 
 double BinaryExpression::evaluate() const
 {
-	switch (operation)
+	try
 	{
-	case '+': return left->evaluate() + right->evaluate(); break;
-	case '-': return left->evaluate() - right->evaluate(); break;
-	case '*': return left->evaluate() * right->evaluate(); break;
-	case '/': return left->evaluate() / right->evaluate(); break; // throw if 0
-	case '^': return pow(left->evaluate(), right->evaluate()); break;
+		switch (operation)
+		{
+		case '+': return left->evaluate() + right->evaluate(); break;
+		case '-': return left->evaluate() - right->evaluate(); break;
+		case '*': return left->evaluate() * right->evaluate(); break;
+		case '/':
+		{
+			if (right->evaluate() == 0) throw std::logic_error("Cannot Divide by 0");
+			return left->evaluate() / right->evaluate();
+			break;
+		}
+		case '^': return pow(left->evaluate(), right->evaluate()); break;
+		}
+	}
+	catch (std::logic_error& ex)
+	{
+		throw;
 	}
 }
 
 Expression* BinaryExpression::clone() const
 {
-	return new BinaryExpression(*this);
+	return new BinaryExpression(operation, left->clone(), right->clone());
+}
+
+
+BinaryExpression::~BinaryExpression()
+{
+	delete left;
+	delete right;
 }
