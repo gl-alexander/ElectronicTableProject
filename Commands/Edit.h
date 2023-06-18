e350de4a8bd88afc08bd8c60b98de6261183b734
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-18 19:54:43 +0300

    improves exception handling

--- Commands/Edit.h
+++ Commands/Edit.h
@@ -1,11 +1,19 @@
-#pragma once
-#include "Command.h"
-class Edit : public Command
-{
-	MyString cellLocation;
-	MyString newValue;
-public:
-	Edit(const MyString& cellLocation, const MyString& newValue);
-	void execute(Table*& table) const override;
-};
+#include "Edit.h"
 
+Edit::Edit(const MyString& cellLocation, const MyString& newValue) : cellLocation(cellLocation), newValue(newValue)
+{}
+
+void Edit::execute(Table*& table) const
+{
+	if(table == nullptr) throw std::logic_error("No table has been opened!");
+	try
+	{
+		table->editCell(cellLocation, newValue);
+	}
+	catch (std::invalid_argument& ex)
+	{
+		throw;
+	}
+
+	std::cout << "Cell edited successfully" << std::endl;
+}
