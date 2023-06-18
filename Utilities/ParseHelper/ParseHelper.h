997430cca4813f25eb154240a9461d580d524807
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-19 00:10:51 +0300

    adds string parsing

--- Utilities/ParseHelper/ParseHelper.h
+++ Utilities/ParseHelper/ParseHelper.h
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <sstream>
 #include "../Validation/Validation.h"
+#pragma warning (disable : 4996)
 
 
 const char DECIMAL_SYMBOL = '.';
+const size_t MAX_INT_LEN = 50;
 
 static StringView extractExpression(const MyString& string)
 {
@@ -28,6 +30,11 @@ static size_t toDigit(char ch)
 {
 	if (ch < '0' || ch > '9') return 0;
 	return ch - '0';
+}
+
+static char digitToChar(size_t digit)
+{
+	return digit + '0';
 }
 
 static size_t intLen(int n)
@@ -129,6 +136,15 @@ static double toDouble(const StringView& str)
 	return decimalRes;
 }
 
+static int countCharOccurances(const char* buffer, char ch)
+{
+	unsigned count = 0;
+	while (*buffer)
+	{
+		if (*(buffer++) == ch) count++;
+	}
+	return count;
+}
 
 
 
@@ -148,4 +164,57 @@ static void getRowAndColumn(const StringView& str, size_t& row, size_t& column)
 	size_t startingInd = intLen(row) + 2; // we need to skip the characters representing the int row as well as the 'R' and 'C'
 	size_t howMany = str.length() - startingInd;
 	column = toInt(str.substr(startingInd, howMany));
+}
+
+
+static const char* intToString(int n)
+{
+	char buffer[MAX_INT_LEN];
+	if (n == 0) buffer[0] = '0';
+	size_t numLen = intLen(n);
+	size_t strEnd = numLen;
+	if (n < 0)
+	{
+		buffer[0] = '-';
+		strEnd++;
+	}
+	
+	while (numLen > 0)
+	{
+		buffer[numLen - 1] = digitToChar(n % 10);
+		numLen--;
+		n /= 10;
+	}
+	buffer[strEnd] = '\0';
+	return buffer;
+}
+
+static MyString parseString(const char* str)
+{
+	char buffer[BUFFER_LEN];
+	size_t current = 0;
+	
+	size_t size = strlen(str);
+	for (int i = 1; i < size - 1; i++)
+	{
+		if (str[i] == '\\' && str[i+1] == '"')
+		{
+			i++;
+		}
+		buffer[current++] = str[i];
+	}
+	buffer[current] = '\0'; // this cuts the string before the last "
+
+	return MyString(buffer);
+}
+
+static void substr(const char* str, size_t beg, size_t len, char* dest)
+{
+	char temp[BUFFER_LEN];
+	for (int i = 0; i < len; i++)
+	{
+		temp[i] = str[beg + i];
+	}
+	temp[len] = 0;
+	strcpy(dest, temp);
 }
