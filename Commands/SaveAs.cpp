432e00e5adf9400c97bcf8e1fac3989a5d45161d
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-18 18:14:19 +0300

    changes syntax error

--- Commands/SaveAs.cpp
+++ Commands/SaveAs.cpp
@@ -5,6 +5,6 @@ SaveAs::SaveAs(const MyString& path) : path(path)
 
 void SaveAs::execute(Table*& table) const
 {
-	if (t == nullptr) throw std::logic_error("No table has been opened");
-	t->saveToFile(path);
+	if (table == nullptr) throw std::logic_error("No table has been opened");
+	table->saveToFile(path);
 }
