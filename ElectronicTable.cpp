d98f2cec0ea3ff3552483f8e16e431371291424f
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-18 19:16:19 +0300

    adds example main

--- ElectronicTable.cpp
+++ ElectronicTable.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include "Factory/CommandFactory.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	Table* table = nullptr;
+	while (true)
+	{
+		try
+		{
+			CommandFactory::getInstance()->createCommand()->execute(table);
+		}
+		catch (std::logic_error& ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
+		catch (exit_exception& ex)
+		{
+			std::cout << ex.what() << std::endl; break;
+		}
+	}
 
+}
