46bfcbc242f8599db186ccd4929d9f3e9f394887
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-19 00:01:17 +0300

    adds console printing for help and info

--- Utilities/PrintHelper/PrintHelper.cpp
+++ Utilities/PrintHelper/PrintHelper.cpp
@@ -1,5 +1,26 @@
 #include "PrintHelper.h"
 
+const double EPSILON = 0.00001;
+
+void PrintHelper::printTitle()
+{
+	std::cout << ">Table Parser Project" << std::endl;
+	std::cout << "by: Alexander Glushkov" << std::endl;
+	std::cout << "_______________________" << std::endl;
+}
+
+void PrintHelper::printHelp()
+{
+	std::cout << "Supported commands:" << std::endl;
+	std::cout << "\topen <filename>" << std::endl;
+	std::cout << "\tprint" << std::endl;
+	std::cout << "\tsave" << std::endl;
+	std::cout << "\tsaveas <filename>" << std::endl;
+	std::cout << "\tedit <R#C#> <new value>" << std::endl;
+	std::cout << "\tclose" << std::endl;
+	std::cout << "\texit" << std::endl;
+}
+
 size_t PrintHelper::intLen(int n)
 {
 	if (n == 0) return 1;
@@ -13,15 +34,37 @@ size_t PrintHelper::intLen(int n)
 	return len;
 }
 
-size_t PrintHelper::doubleLen(double d, size_t decimalPlaces)
+static size_t decimalPlaces(double d)
 {
-	int converted = (int)d;
-
-	size_t len = intLen(converted);
-	
-	len += decimalPlaces + 1; // +1 because we need to print the decimal point .
-
+	size_t len = 0;
+	d -= int(d);
+	while (d >= EPSILON)
+	{
+		d *= 10;
+		d -= int(d);
+		len++;
+		
+	}
 	return len;
+}
+
+size_t PrintHelper::doubleLen(double num)
+{
+	int converted = (int)num;
+	if (abs(num - converted) < EPSILON) return intLen(converted); // the double is the same as the its int representation
+
+
+	size_t len = 0;
+	if (num < 0) len++;
+	num = abs(num);
+	
+	len += intLen(converted);
+
+	double difference = num - converted;
+	
+	size_t places = decimalPlaces(abs(difference));
+	
+	return len + places + 1; // +1, counting the Decimal point
 }
 
 void PrintHelper::printCharNTimes(char ch, int count, std::ostream& os)
--- Utilities/PrintHelper/PrintHelper.h
+++ Utilities/PrintHelper/PrintHelper.h
@@ -6,9 +6,11 @@
 
 struct PrintHelper
 {
-	static const unsigned DECIMAL_PLACES_TO_PRINT = 3;
+	static void printTitle();
+	static void printHelp();
+
 	static size_t intLen(int n);
-	static size_t doubleLen(double d, size_t decimalPlaces = DECIMAL_PLACES_TO_PRINT);
+	static size_t doubleLen(double d);
 
 	static void printCharNTimes(char ch, int count, std::ostream& os);
 	static void printWhitespaces(int count, std::ostream& os);
