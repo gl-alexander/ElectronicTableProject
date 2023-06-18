7c96c2a0b349a3db82e2ddaf8fcccba60fab2756
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-18 18:19:26 +0300

    adds nullptr check

--- Commands/Edit.cpp
+++ Commands/Edit.cpp
@@ -5,6 +5,7 @@ Edit::Edit(const MyString& cellLocation, const MyString& newValue) : cellLocatio
 
 void Edit::execute(Table*& table) const
 {
+	if(table == nullptr) throw std::logic_error("No table has been opened!");
 	try
 	{
 		table->editCell(cellLocation, newValue);
