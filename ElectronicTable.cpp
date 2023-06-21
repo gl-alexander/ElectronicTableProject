45d4ca38abde1d9313fb5674276c3e7a2e7c0046
Alexander Glushkov <31550731+gl-alexander@users.noreply.github.com>
2023-06-21 21:23:33 +0300

    adds invalid argument catching and comments

--- ElectronicTable.cpp
+++ ElectronicTable.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
+	PrintHelper::printTitle();
+	PrintHelper::printHelp();
 	Table* table = nullptr;
 	while (true)
 	{
@@ -10,7 +12,15 @@ int main()
 		{
 			CommandFactory::getInstance()->createCommand()->execute(table);
 		}
-		catch (std::logic_error& ex)
+		catch (std::runtime_error& ex) // invalid values 
+		{
+			std::cout << ex.what() << std::endl; break;
+		}
+		catch (std::logic_error& ex) // invalid formula expression
+		{
+			std::cout << ex.what() << std::endl;
+		}
+		catch (std::invalid_argument& ex) // invalid value passed
 		{
 			std::cout << ex.what() << std::endl;
 		}
